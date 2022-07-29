7c4230e36fbc026e004e7de7e9c9dbda16b463ba
Filipe Oliveira <filipe.oliveira@contabilizei.com.br>
2022-07-29 00:33:25 -0300

    feat: creating container with list of pokemons

---
+++ src/_services/pokemon.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Pokemon } from '../_model/Pokemon';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+
+  public pokemons: Pokemon[] = [];
+
+  constructor( private httpClient: HttpClient ) {
+    const endpointPokeCards = 'https://api.pokemontcg.io/v2/cards/?pageSize=20&orderBy=name';
+
+    this.httpClient.get<any>(endpointPokeCards)
+      .subscribe(result => this.pokemons = result.data);
+      console.log(this.pokemons);
+  }
+}
