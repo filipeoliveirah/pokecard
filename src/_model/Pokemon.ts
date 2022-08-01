0d4912ffca2e527424298f3fd5ee27859c915c1c
Filipe Oliveira <filipe.oliveira@contabilizei.com.br>
2022-08-01 14:25:47 -0300

    fix: remove useless function

--- src/_model/Pokemon.ts
+++ src/_model/Pokemon.ts
@@ -7,10 +7,6 @@ export interface Pokemon {
   types: Type[];
 }
 
-export function getPokemonImage(pokemon: Pokemon): string {
-  return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${getPokemonNumber(pokemon)}.png`;
-}
-
 export function getPokemonNumber(pokemon: Pokemon): string {
   return leadingZero(pokemon.number);
 }
